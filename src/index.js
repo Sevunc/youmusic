434e1276312061491152f2de7877260c0968b30c
Sevunc <61692802+Sevunc@users.noreply.github.com>
2020-05-29 04:02:26 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Search />
   </React.StrictMode>,
   document.getElementById('root')
 );
